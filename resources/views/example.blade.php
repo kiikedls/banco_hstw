f57b5cf37672df8ec7766fd68e500938f120d63f
Juan Angel Reyes <juangel2406@gmail.com>
2019-11-23 11:48:18 -0600

    Pa' mis compas

---
+++ resources/views/example.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.interface')
+@section('CSS')
+<!--
+Aquí linkearás el CSS generado por SASS
+<link rel="stylesheet" href="css/example.css"> -->
+@endsection
+
+@section('content')
+<!--
+Esta es la sección principal donde incluirás tu trabajo.
+
+El contenido esta por debajo del Navbar de la plantilla y
+esta dentro de un <div class="container-fluid mt-4" id="container">
+-->
+@endsection
+
+
+@section('JS')
+<!--
+Puedes añadir código JavaScript y librerías.
+
+Tu código aquí no afectará el trabajo de los demás
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript">
+</script>
+-->
+@endsection
