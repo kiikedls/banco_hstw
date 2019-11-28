c262cfe3a05e1c265c6103ddbaef895a8fd83f8c
Raul Alejandro <dronerlira@gmail.com>
2019-11-27 18:07:17 -0600

    Subir

--- resources/views/burodecredito.blade.php
+++ resources/views/burodecredito.blade.php
@@ -8,85 +8,78 @@
   <div class="card container-fluid">
 
   <div class="container">
-    <div class="row">
-      <div class="col">
-        Buro de Credito
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <form class="form-inline active-cyan-4">
-          <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
-          <i class="fas fa-search" aria-hidden="true"></i>
-          <!-- Material inline 1 -->
-        <div class="form-check form-check-inline">
-          <input type="radio" class="form-check-input" id="materialInline1" name="inlineMaterialRadiosExample">
-          <label class="form-check-label" for="materialInline1">1</label>
-        </div>
-
-        <!-- Material inline 2 -->
-        <div class="form-check form-check-inline">
-          <input type="radio" class="form-check-input" id="materialInline2" name="inlineMaterialRadiosExample">
-          <label class="form-check-label" for="materialInline2">2</label>
-        </div>
-
-        <!-- Material inline 3 -->
-        <div class="form-check form-check-inline">
-          <input type="radio" class="form-check-input" id="materialInline3" name="inlineMaterialRadiosExample">
-          <label class="form-check-label" for="materialInline3">3</label>
-        </div>
-        </form>
-      </div>
-    </div>
-  </div>
-
-  <div class="table-responsive">
-      <!--Table-->
-      <table class="table table-striped">
-
-      <!--Table head-->
-      <thead>
-        <tr>
-          <th>Folio</th>
-          <th>Nombre</th>
-          <th>Fecha de Nacimiento</th>
-          <th>Rfc</th>
-          <th>Fecha de Regsitro de BC</th>
-          <th>Position</th>
-        </tr>
-      </thead>
-      <!--Table head-->
-
-      <!--Table body-->
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Kate</td>
-          <td>Moss</td>
-          <td>USA / The United Kingdom / China / Russia </td>
-          <td>New York City / Warsaw / Lodz / Amsterdam / London / Chicago</td>
-          <td>Web Designer /UX designer / Ul designer / JavaScript Developer</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Anna</td>
-          <td>Wintour</td>
-          <td>USA / The United Kingdom / China / Russia </td>
-          <td>New York City / Warsaw / Lodz / Amsterdam / London / Chicago</td>
-          <td>Web Designer /UX designer / Ul designer / JavaScript Developer</td>
-        </tr>
-        <tr>
-          <th scope="row">4</th>
-          <td>Jerry</td>
-          <td>Horwitz</td>
-          <td>USA / The United Kingdom / China / Russia </td>
-          <td>New York City / Warsaw / Lodz / Amsterdam / London / Chicago</td>
-          <td>Web Designer /UX designer / Ul designer / JavaScript Developer</td>
-        </tr>
-      </tbody>
-      <!--Table body-->
-      </table>
-      <!--Table-->
+<table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+  <thead>
+    <tr>
+      <th class="th-sm text-center">Folio
+      </th>
+      <th class="th-sm text-center">Nombre
+      </th>
+      <th class="th-sm text-center">Fecha de Nacimiento
+      </th>
+      <th class="th-sm text-center">Fecha de Registro Buro de Credito
+      </th>
+      <th class="th-sm text-center">Estado
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Raul Alejandro</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i id="" class="fas fa-smile-beam fa-lg success-color-dark-text"></i>
+      </td>
+    </tr>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Diana Laura</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i class="fas fa-exclamation-triangle fa-lg danger-color-dark-text"></i>
+      </td>
+    </tr>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Martha Herrrera</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i id="" class="fas fa-smile-beam fa-lg text-success"></i>
+      </td>
+    </tr>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Jhon Snow</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i class="fas fa-exclamation-circle fa-lg text-warning"></i>
+      </td>
+    </tr>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Pitoro Daimacu</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i class="fas fa-exclamation-triangle fa-lg text-danger"></i>
+      </td>
+    </tr>
+    <tr class="text-center">
+      <td>103923</td>
+      <td>Carl Jhonson</td>
+      <td>2011/04/25</td>
+      <td>2011/04/25</td>
+      <td>
+        <i class="fas fa-exclamation-triangle fa-lg text-danger"></i>
+      </td>
+    </tr>
+  </tbody>
+</table>
   </div>
 
   </div>
@@ -96,7 +89,8 @@
 @section('JS')
 <script>
   $(document).ready(function () {
-
-});
+    $('#dtBasicExample').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+  });
 </script>
 @endsection
