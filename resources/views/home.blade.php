50066b787e4028432a7cff7e96f53ea4a7e0a774
Angel <juangel2406@gmail.com>
2019-11-30 11:36:28 -0600

    Probando VS Code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,50 +6,44 @@
 
 @section('content')
 
-<div class="view intro-2" style="">
-	<div class="full-bg-img">
-		<div class="mask flex-center">
-			<div class="container text-center white-text wow fadeInUp">
-				<h2 class="display-1 font-weight-bold">Bienvenido al SWPC</h2>
-				<br>
-				<h5>Sistema web de préstamos y cobranza</h5>
-				En este sitio podrás desempeñarte como trabajador de <b>HSTW Banco</b> de forma más ágil y rápida
-				<br>
-				<a class="btn btn-lg btn-outline-white mt-5 mx-auto" href="#">EMPEZAR</a>
-			</div>
-		</div>
-	</div>
+<div class="view intro-2">
+    <div class="full-bg-img">
+        <div class="mask flex-center">
+            <div class="container text-center white-text wow fadeInUp">
+                <h2 class="display-1 font-weight-bold">Bienvenido al SWPC</h2>
+                <br>
+                <h5>Sistema web de préstamos y cobranza</h5>
+                En este sitio podrás desempeñarte como trabajador de <b>HSTW Banco</b> de forma más ágil y rápida
+                <br>
+                <a class="btn btn-lg btn-outline-white mt-5 mx-auto" href="#">EMPEZAR</a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="container">
-	<div class="row">
-		<div class="col-md-12">
-
-			<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-				ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
-				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-				enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.</p>
-
-		</div>
-	</div>
+    <p class="text-justify my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.</p>
 </div>
 
 @endsection
 
 @section('JS')
 <script type="text/javascript">
-	var nav = $('.navbar');
+    var nav = $('.navbar');
 
-	nav.removeClass('blue-gradient');
-	nav.addClass('fixed-top z-depth-0');
+    nav.removeClass('blue-gradient');
+    nav.addClass('fixed-top z-depth-0');
 
-	$('#container').removeClass('container-fluid mt-4');
+    $('#container').removeClass('container-fluid mt-4');
 
 </script>
 @endsection
